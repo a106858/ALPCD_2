ffa714e381e476bb214638d1d3fc27a86efdb2f3
a106858 <a106858@alunos.uminho.pt>
2024-11-05 16:16:13 +0000

    upload do ficheiro python

---
+++ projeto.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://api.itjobs.pt/job/list.json?api_key=09ad1042ebaf1704533805cd2fab64f1"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
