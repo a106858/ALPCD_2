9f3b4088befe8149e97e9eb068cf87f7debed704
a106858 <a106858@alunos.uminho.pt>
2024-12-30 01:08:12 +0000

    completo

--- projeto2.py
+++ projeto2.py
@@ -7,6 +7,7 @@ import webbrowser
 import csv
 from collections import defaultdict
 from typing import Optional
+from collections import Counter
 
 app = typer.Typer()
 
@@ -215,12 +216,23 @@ def export_to_csv(data, export_csv: Optional[str]):
     if export_csv:
         try:
             with open(export_csv, mode='w', newline='', encoding='utf-8') as csv_file:
-                fieldnames = data.keys()
-                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerow(data)
+                if isinstance(data, dict):
+                    # caso de dados simples 
+                    fieldnames = data.keys()
+                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerow(data)
+                elif isinstance(data, list) and all(isinstance(item, dict) for item in data):
+                    # caso de lista de dicionários 
+                    fieldnames = data[0].keys() if data else []
+                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerows(data)
+                else:
+                    raise ValueError("Formato de dados não suportado para exportação.")
 
             print(f"Dados exportados para o arquivo CSV: {export_csv}")
+        
         except Exception as e:
             print(f"Erro ao exportar dados para CSV: {e}")
 
@@ -313,6 +325,165 @@ def statistics(
                 typer.echo(f"Zona: {zone}, Tipo de Trabalho: {job_type}, Nº de Vagas: {number}")
         else:
             typer.echo(f"Nenhuma vaga encontrada para o trabalho '{job_title}'.")
+
+# função para ir buscar os links dos jobs na página
+def get_job_urls(job_title: str):
+    job_title_formatted = re.sub(r'\s+', '%20', job_title.lower())
+    url = f"https://www.ambitionbox.com/jobs/search?tag={job_title_formatted}"
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0", 
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
+               "Accept-Language": "en-US,en;q=0.5", 
+               "Accept-Encoding": "gzip, deflate", 
+               "Connection": "keep-alive", 
+               "Upgrade-Insecure-Requests": "1", 
+               "Sec-Fetch-Dest": "document", 
+               "Sec-Fetch-Mode": "navigate", 
+               "Sec-Fetch-Site": "none", 
+               "Sec-Fetch-User": "?1", 
+               "Cache-Control": "max-age=0"}
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    job_elements = soup.find_all('div', class_='jobsInfoCardCont')
+
+    job_urls = [f"https://www.ambitionbox.com{job.find('a')['href']}" for job in job_elements]
+
+    return job_urls
+
+# função para encontrar skills de um job específico
+def get_skills_from_job(job_url: str):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0", 
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", 
+               "Accept-Language": "en-US,en;q=0.5", 
+               "Accept-Encoding": "gzip, deflate", 
+               "Connection": "keep-alive", 
+               "Upgrade-Insecure-Requests": "1", 
+               "Sec-Fetch-Dest": "document", 
+               "Sec-Fetch-Mode": "navigate", 
+               "Sec-Fetch-Site": "none", 
+               "Sec-Fetch-User": "?1", 
+               "Cache-Control": "max-age=0"}
+
+    response = requests.get(job_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    skills_elements = soup.find_all('a', class_='body-medium chip')
+
+    skills = [skill.get_text(strip=True).lower() for skill in skills_elements]
+
+    return skills
+
+# função para listar as principais skills de um trabalho
+@app.command()
+def list_skills(job_title: str, export_csv: Optional[str] = None):
+    try:
+        job_urls = get_job_urls(job_title)
+
+        all_skills = []
+
+        for url in job_urls:
+            skills = get_skills_from_job(url)
+            all_skills.extend(skills)
+
+        skill_count = Counter(all_skills)
+
+        top_skills = skill_count.most_common(10)
+
+        result = [{"skill": skill, "count": count} for skill, count in top_skills]
+
+        print(json.dumps(result, indent=4))
+
+        # exportar dados para csv (opcional)
+        export_to_csv(result, export_csv)
+
+    except Exception as e:
+        print(f"Erro: {e}")
+
+# função para obter o html da empresa do website indeed.com
+def get_html2(job_id):
+    company_name = find_company_name(job_id)
+    if company_name is None:
+        print(f"Empresa não encontrada para o job ID {job_id}.")
+        return
+    
+    company_name1 = re.sub(r'\s+', '-', company_name)
+    print(company_name1)
+    url = f"https://pt.indeed.com/cmp/{company_name1}"
+    
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+               "Accept-Language": "pt-PT,pt;q=0.9,pt-BR;q=0.8,en;q=0.7,en-US;q=0.6,en-GB;q=0.5",
+               "Accept-Encoding": "gzip, deflate, br, zstd",
+               "Cache-Control": "max-age=0",
+               "Sec-Fetch-Dest": "document", 
+               "Sec-Fetch-Mode": "navigate", 
+               "Sec-Fetch-Site": "cross-site", 
+               "Sec-Fetch-User": "?1"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200: 
+        print(f"Website não encontrado para a empresa: {company_name}.")
+        return None
+    
+    soup = BeautifulSoup(response.text, 'lxml')
+    
+    return soup
+
+# função para obter o conteúdo da empresa em formato json através do html
+def get_html_content2(soup):
+    if soup is None:
+        return {
+            "overall_rating": "N/A",
+            "description": "Informações indisponíveis.",
+        }
+
+    body = soup.find('body')
+
+    if not body:
+        return {
+            "overall_rating": "N/A",
+            "description": "Informações indisponíveis.",
+        }
+    
+    # encontrar o overall rating
+    overall_rating = body.find('span', attrs={"aria-hidden":"true"}, class_="css-4f2law e1wnkr790").text  
+    overall_rating_1 = overall_rating.text if overall_rating else "N/A"
+
+    # encontrar a descrição da empresa
+    description_div = body.find('div', attrs={"data-testid": "less-text", "class": "css-9146s eu4oa1w0"})     # em alguns casos é more_text, alterar
+    if description_div:
+        paragraphs = description_div.find_all('p')
+        description = " ".join(p.text for p in paragraphs)
+    else:
+        description = "Informações indisponíveis."
+    
+    html_data = {
+        "overall_rating": overall_rating_1,
+        "description": description,        
+    }
+    return html_data
+
+# função para juntar todas as informações
+@app.command()
+def informations2(
+    job_id: int, 
+    export_csv: Optional[str] = typer.Option(None, help="Exporta os dados para um arquivo CSV"),
+):
+    api_data = get_api_content(job_id)
+    if api_data is None:
+        print(f"Job com ID {job_id} não encontrado na API.")
+        return
+    soup = get_html2(job_id)
+    html_content = get_html_content2(soup)
+
+    data = {**api_data, **html_content}
+    print(json.dumps(data, indent=4))
+    
+    # exportar dados para csv (opcional)
+    if export_csv:
+        export_to_csv(data, export_csv)
 
 if __name__ == "__main__":
     app()
